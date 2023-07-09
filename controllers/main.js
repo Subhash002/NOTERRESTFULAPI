9bf8f33a532a6c111b529578753d5938b34d37f2
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-07-09 12:18:32 +0530

    updated API

--- controllers/main.js
+++ controllers/main.js
@@ -1,3 +1,4 @@
+import { validationResult } from "express-validator";
 import { nanoid } from "nanoid";
 let notes = [];
 export const updateNote = (req, res) => {
@@ -33,6 +34,11 @@ export const deleteNote = (req, res) => {
 };
 
 export const createNewNote = (req, res) => {
+  const errors = validationResult(req);
+  console.log(errors);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   const note = req.body;
   notes.push({ ...note, id: nanoid() });
   res.send(`Created note ${note}`);
--- routes/main.js
+++ routes/main.js
@@ -6,10 +6,17 @@ import {
   deleteNote,
   updateNote,
 } from "../controllers/main.js";
+import { body } from "express-validator";
 const router = express.Router();
 
 router.get("/", getAllNotes);
-router.post("/", createNewNote);
+router.post(
+  "/",
+  body("title").exists(),
+  body("content").exists(),
+  body("isDraft").exists().isBoolean(),
+  createNewNote
+);
 router.get("/:id", getNoteById);
 router.delete("/:id", deleteNote);
 router.patch("/:id", updateNote);
